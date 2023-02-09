5d3587a8365644192c328fb9ed8a59704916d32a
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-02-08 19:07:07 -0500

    added ejs files

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.static(path_1.default.join(__dirname, 'Public')));
 app.use('/', index_1.default);
+
 //app.use('/users', users_1.default);
 app.use(function (req, res, next) {
     next((0, http_errors_1.default)(404));
