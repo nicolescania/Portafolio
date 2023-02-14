715a269d549c6caa9fc6cec7d3715372d74ec4b0
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-02-14 01:14:12 -0500

    added movie modules

---
+++ Server/Models/movie.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema
+({
+
+    Name: String,
+    Year: String,
+    Director: String,
+    Rating: String
+
+},
+{
+    collection: "movies"
+});
+
+
+const Model = mongoose.model("Movies", MovieSchema);
+export default Model;
