067c708ce834f23bd34f152cf57901eddc599715
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-02-24 17:20:10 -0500

    modified Navbar for auth

--- Server/Controllers/auth.ts
+++ Server/Controllers/auth.ts
@@ -13,12 +13,12 @@ import User from '../Models/user'
 
 export function DisplayLoginPage(req: Express.Request, res:express.Response, next:express.NextFunction)
 {
-        res.render('index', { title: 'Login', page: 'login', messages: req.flash('loginMessage'), distplayName: ''});
+        res.render('index', { title: 'Login', page: 'login', messages: req.flash('loginMessage'), displayName: ''});
 }
 
 export function DisplayRegisterPage(req: Express.Request, res:express.Response, next:express.NextFunction)
 {
-    res.render('index', { title: 'Register', page: 'register', messages: req.flash('registerMessage'), distplayName: ''});
+    res.render('index', { title: 'Register', page: 'register', messages: req.flash('registerMessage'), displayName: ''});
 }
 
 //Processing Functions
