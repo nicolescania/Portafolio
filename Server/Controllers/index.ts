3f78d6dc29dde1f676880f813961921e55fcb54d
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-02-28 15:59:43 -0500

    added utility functions

--- Server/Controllers/index.ts
+++ Server/Controllers/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 
 export function DisplayHomePage(req: Express.Request, res:express.Response, next:express.NextFunction)
 {
-    res.render('index', { title: 'Home', page: 'home', displayName: ' '});
+    res.render('index', { title: 'Home', page: 'home', displayName: '', hola:"hola"});
 }
 
 
