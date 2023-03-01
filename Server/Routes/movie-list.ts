019f8be4b0d0980e815df5a876c59e373e1b2126
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-03-01 00:50:54 -0500

    Update movie-list router

--- Server/Routes/movie-list.ts
+++ Server/Routes/movie-list.ts
@@ -3,9 +3,25 @@ const router = express.Router();
 
 import { AuthGuard } from '../../Util';
 
-import { DisplayMovieListPage } from '../Controllers/movie-list';
+import { DisplayMovieListPage, DisplayAddPage, DisplayEditPage, ProcessAddPage, ProcessEditPage, ProcessDeletePage } from '../Controllers/movie-list';
 
 /* Display Movie List Page */
 router.get('/movie-list', AuthGuard, DisplayMovieListPage);
+
+
+// Display Add Page
+router.get('/add', AuthGuard, DisplayAddPage);
+
+// Display Edit Page
+router.get('/edit/:id', AuthGuard, DisplayEditPage);
+
+// Process Add Page
+router.post('/add', AuthGuard, ProcessAddPage);
+
+// Process Edit Page
+router.post('/edit/:id', AuthGuard, ProcessEditPage);
+
+// Process Delete Page
+router.get('/delete/:id', AuthGuard, ProcessDeletePage);
 
 export default router;
