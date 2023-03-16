aeb8f6d70f023d57eeb76ca6330140dc421defd5
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-03-16 13:34:12 -0400

    updated db config

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,6 +1,6 @@
 export const LocalURI = "mongodb://127.0.0.1/media";
-//export const RemoteURI = "mongodb+srv://Nicolescania:dx6fLBhoa7B413yB@cluster0.suah0sg.mngodb.net/media?retryWrites=true&w=majority/";
-export const RemoteURI = process.env.remoteURI;
+export const RemoteURI = "mongodb+srv://Nicolescania:dx6fLBhoa7B413yB@cluster0.suah0sg.mngodb.net/media?retryWrites=true&w=majority/";
+//export const RemoteURI = process.env.remoteURI;
 
 export const HostName = "MongoDB Atlas";
 export const Secret = "someSecret";
