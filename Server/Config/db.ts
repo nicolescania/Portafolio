957b1f39b1d31f8d57672ed0957b2710d64158d8
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-03-23 14:19:28 -0400

    updated folders name

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,7 +1,6 @@
 export const LocalURI = "mongodb://127.0.0.1/media";
 export const RemoteURI = "mongodb+srv://nicole:Jti58XJccURbz4QH@cluster1.ohs5nel.mongodb.net/media?retryWrites=true&w=majority";
-//export const RemoteURI = process.env.remoteURI;
-//export const RemoteURI = "mongodb+srv://robertgescario:IgvPEfhQURUk479v@cluster0.3aejz5z.mongodb.net/media?retryWrites=true&w=majority";
+
 
 export const HostName = "MongoDB Atlas";
 
