380413887e775720b0f2a3e8b9f56a6257faac04
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-06-17 15:55:17 -0400

    updated resume link

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -2,8 +2,6 @@ export const LocalURI = "mongodb://127.0.0.1/media";
 export const RemoteURI = "mongodb+srv://nicole:Jti58XJccURbz4QH@cluster1.ohs5nel.mongodb.net/media?retryWrites=true&w=majority";
 
 
-
-
 export const HostName = "MongoDB Atlas";
 
 export const Secret = "someSecret";
